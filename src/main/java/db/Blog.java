f3fbe262a222ce2a5d7ba49011d3cc02a6b712e8
Christophe <christophe@qudini.com>
2018-09-26 11:33:54 +0100

    Entities improved

--- src/main/java/db/Blog.java
+++ src/main/java/db/Blog.java
@@ -3,20 +3,12 @@ package db;
 import java.util.ArrayList;
 import java.util.List;
 
-public final class Blog {
+public final class Blog extends Entity {
 
-    public final int id;
-    public final String title;
     public final List<Article> articles = new ArrayList<>();
 
     public Blog(int id) {
-        this.id = id;
-        this.title = "Blog #" + id;
-    }
-
-    @Override
-    public String toString() {
-        return title;
+        super(id);
     }
 
 }
--- src/main/java/db/Comment.java
+++ src/main/java/db/Comment.java
@@ -1,21 +1,13 @@
 package db;
 
-public final class Comment {
+public final class Comment extends Entity {
 
-    public final int id;
-    public final String title;
     public final Article article;
 
     public Comment(int id, Article article) {
-        this.id = id;
-        this.title = "Comment #" + id;
+        super(id);
         this.article = article;
         article.comments.add(this);
-    }
-
-    @Override
-    public String toString() {
-        return title;
     }
 
 }
