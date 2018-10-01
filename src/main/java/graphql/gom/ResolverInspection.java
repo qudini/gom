6a57849f133bb3f8cdf3556766fd6208e9121506
Christophe <christophe@qudini.com>
2018-10-01 16:00:43 +0100

    ResolverMethodInvoker

--- src/main/java/graphql/gom/ResolverInspection.java
+++ src/main/java/graphql/gom/ResolverInspection.java
@@ -1,7 +1,6 @@
 package graphql.gom;
 
 import graphql.gom.utils.Maps;
-import graphql.gom.utils.Methods;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.dataloader.DataLoader;
@@ -25,39 +24,11 @@ import static org.dataloader.DataLoader.newMappedDataLoader;
 @Getter(PACKAGE)
 final class ResolverInspection<C extends DataLoaderRegistryGetter> {
 
-    private final Converters<C> converters;
+    private final ResolverMethodInvoker<C> resolverMethodInvoker;
 
     private final Set<FieldWiring> fieldWirings = new HashSet<>();
 
     private final Set<DataLoaderRegistrar> dataLoaderRegistrars = new HashSet<>();
-
-    private <R> CompletableFuture<R> invoke(Method method, Object instance, Object source, Arguments arguments, C context) {
-        final Object returnedValue;
-        switch (method.getParameterCount()) {
-            case 0:
-                if (source == null) {
-                    returnedValue = Methods.invoke(method, instance);
-                } else {
-                    throw new IllegalStateException(); // FIXME
-                }
-                break;
-            case 1:
-                returnedValue = source == null
-                        ? Methods.invoke(method, instance, arguments)
-                        : Methods.invoke(method, instance, source);
-                break;
-            case 2:
-                if (source == null) {
-                    throw new IllegalStateException(); // FIXME
-                } else {
-                    returnedValue = Methods.invoke(method, instance, source, arguments);
-                }
-                break;
-            default:
-                throw new IllegalStateException(); // FIXME
-        }
-        return converters.convert(returnedValue, context);
-    }
 
     private <S, R> void createBatchedFieldWiring(String type, String field, Method method, Object instance) {
         String dataLoaderKey = randomUUID().toString();
@@ -76,7 +47,7 @@ final class ResolverInspection<C extends DataLoaderRegistryGetter> {
                                 .getValue()
                                 .stream()
                                 .collect(toMap(DataLoaderKey::getSource, identity()));
-                        return this
+                        return resolverMethodInvoker
                                 .<Map<S, R>>invoke(
                                         method,
                                         instance,
@@ -128,7 +99,7 @@ final class ResolverInspection<C extends DataLoaderRegistryGetter> {
         fieldWirings.add(new FieldWiring<>(
                 type,
                 field,
-                environment -> invoke(
+                environment -> resolverMethodInvoker.invoke(
                         method,
                         instance,
                         environment.getSource(),
@@ -163,7 +134,8 @@ final class ResolverInspection<C extends DataLoaderRegistryGetter> {
     }
 
     static <C extends DataLoaderRegistryGetter> ResolverInspection<C> inspect(Collection<Object> resolvers, Converters<C> converters) {
-        ResolverInspection<C> inspector = new ResolverInspection<>(converters);
+        ResolverMethodInvoker<C> resolverMethodInvoker = new ResolverMethodInvoker<>(converters);
+        ResolverInspection<C> inspector = new ResolverInspection<>(resolverMethodInvoker);
         resolvers.forEach(inspector::inspect);
         return inspector;
     }
